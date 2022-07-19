f8059ad3a2103253bd57f5e260e538eedadd8a51
zokekai <zokekai@gmail.com>
2022-07-20 03:14:37 +0800

    Desc:引入junit jar包

--- src/test/java/com/zoke/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/zoke/springboot/SpringbootApplicationTests.java
@@ -1,7 +1,7 @@
 package com.zoke.springboot;
 
 import com.zoke.springboot.domain.User;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
